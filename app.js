65a4cf1e06ee7dbc466eb389e36c964d120fa86a
Sandy Wright <sandy_wright@btinternet.com>
2017-09-15 13:49:50 +0100

    Added GET Access Control Allow Header

--- app.js
+++ app.js
@@ -62,7 +62,7 @@ app.use(function(req, res, next) {
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 	res.header('Access-Control-Allow-Credentials', true);
 	if(req.method === "OPTIONS") {
-		res.header("Access-Control-Allow-Methods", "PUT,POST,DELETE");
+		res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
 		return res.status(200).json({});
 	}
 	next();
