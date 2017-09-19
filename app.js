1e2f0027f51b80af2548ea2e9391f1e2740b2255
Sandy Wright <sandy_wright@btinternet.com>
2017-09-19 10:01:14 +0100

    options if

--- app.js
+++ app.js
@@ -59,11 +59,12 @@ app.use(function(req, res, next) {
 // http://localhost:3000
 // http://patientfinder.s3-website.eu-west-2.amazonaws.com
 app.use(function(req, res, next) {
-	res.header("Access-Control-Allow-Origin", '*');
-	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-	res.header('Access-Control-Allow-Credentials', false);
 	if(req.method === "OPTIONS") {
 		res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+		res.header("Access-Control-Allow-Origin", '*');
+		res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+		res.header('Access-Control-Allow-Credentials', true);
+	
 		return res.status(200).json({});
 	}
 
