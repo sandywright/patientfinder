8464f463667783aed6e21347c42dcd7d25867c8b
Sandy Wright <sandy_wright@btinternet.com>
2017-09-19 09:37:32 +0100

    *

--- app.js
+++ app.js
@@ -62,7 +62,7 @@ app.use(function(req, res, next) {
 // http://localhost:3000
 // http://patientfinder.s3-website.eu-west-2.amazonaws.com
 app.use(function(req, res, next) {
-	res.header("Access-Control-Allow-Origin", 'http://localhost:3000');
+	res.header("Access-Control-Allow-Origin", '*');
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 	res.header('Access-Control-Allow-Credentials', true);
 	if(req.method === "OPTIONS") {
