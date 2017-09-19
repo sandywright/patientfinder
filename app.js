dfe231520dca1891c12bd842c0c75ae6d20baef9
Sandy Wright <sandy_wright@btinternet.com>
2017-09-19 10:02:50 +0100

    removed wildcard

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ app.use(function(req, res, next) {
 app.use(function(req, res, next) {
 	if(req.method === "OPTIONS") {
 		res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-		res.header("Access-Control-Allow-Origin", '*');
+		res.header("Access-Control-Allow-Origin", 'http://localhost:3000');
 		res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 		res.header('Access-Control-Allow-Credentials', true);
 	
