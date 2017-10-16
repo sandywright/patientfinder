fce81e92b118898238f914231872eeca575d071b
Sandy Wright <sandy_wright@btinternet.com>
2017-10-16 18:50:13 +0100

    headers

--- app.js
+++ app.js
@@ -50,6 +50,7 @@ app.use(function(req, res, next) {
 	res.header('Access-Control-Allow-Credentials', true);
 	if(req.method === "OPTIONS") {
 		res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+		res.header("Access-Control-Allow-Headers": "Authorization")
 		return res.status(200).json({});
 	}
 
